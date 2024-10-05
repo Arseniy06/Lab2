b25a5658c85971f3184f9cb201915f2697d9acca
Arseniy06 <44524793+Arseniy06@users.noreply.github.com>
2024-10-05 13:24:31 +0300

    Add files via upload

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int n = 0;
 int main(){
+    for (int i = 1; i<10;++i){
+        cout<<i<<" x "<<7<<" = "<<7*i<<endl;
+    }
     cin>>n;
     for (int i = 1; i<10;++i){
         cout<<i<<" x "<<n<<" = "<<n*i<<endl;
